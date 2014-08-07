4f54ff3530947647a8aee513eb928940a2509d54
phytax <otto@planet.ms>
2014-08-07 21:51:17 +0200

    Unit Prefs implemented

--- app/src/main/java/de/intralux/sunshine/ForecastFragment.java
+++ app/src/main/java/de/intralux/sunshine/ForecastFragment.java
@@ -244,6 +244,18 @@ public class ForecastFragment extends Fragment {
          * Prepare the weather high/lows for presentation.
          */
         private String formatHighLows(double high, double low) {
+            SharedPreferences sharedPrefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPrefs.getString(getString(R.string.pref_unit_key),
+                getString(R.string.pref_unit_metric));
+
+            if(unitType.equals(getString(R.string.pref_unit_imperial))){
+                high = (high * 1.8) + 32;
+                low = (low * 1.8) + 32;
+            } else if (!unitType.equals(getString(R.string.pref_unit_imperial))) {
+                Log.d(LOG_TAG,"Einheit nicht gefunden: " + unitType);
+            }
+
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
